33acf5083c6fb9ae12bf2ac68ffdb61075eef7df
Michael McKechney <michael@mckechney.com>
2024-02-12 09:19:27 -0500

    fixing logging to keep spacing

--- SpeechAnalyticsLibrary/CustomConsoleFormatter.cs
+++ SpeechAnalyticsLibrary/CustomConsoleFormatter.cs
@@ -64,7 +64,7 @@ namespace SpeechAnalyticsLibrary
             var colorString = message.Split("**COLOR:")[1];
             if (Enum.TryParse(colorString, out color))
             {
-               return (color, message.Split("**COLOR:")[0].Trim());
+               return (color, message.Split("**COLOR:")[0]);
             }
          }
          return (color, message);
